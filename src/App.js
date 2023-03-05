5973abd0455de7fce0f15f092278ae7c5c52c8f8
My Name <cpgsom819@gmail.com>
2023-03-05 23:10:36 +0800

    adding form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { getAuth } from 'firebase/auth';
+import Apps from './Firebase/firebase.init';
+
+
 
 function App() {
+
+  const auth = getAuth(Apps)
+
+  const handleRegister = (event) => {
+
+    event.preventDefault()
+
+    const email = event.target.email.value;
+
+    const password = event.target.password.value;
+
+    console.log(email, password);
+
+
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form  onSubmit={handleRegister}>
+
+        <input type="email" name="email" id=""   placeholder='email'/>
+        <br/>
+        <input type="password" name="password" id="" placeholder='password' />
+
+        <br/>
+
+        <button type='submit'>Register</button>
+      
+      </form>
+       
     </div>
   );
 }
--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const Apps = initializeApp(firebaseConfig);
 
-export default app;
+export default Apps;
