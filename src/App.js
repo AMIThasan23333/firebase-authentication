e294b067fd7755b42e58cbeac7bd8b2bf2892650
My Name <cpgsom819@gmail.com>
2023-03-07 10:13:51 +0800

    initializing tailwind

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import BasicExample from './Register/ReactBootstrap';
-import ReactBootstrap from './Register/ReactBootstrap';
-import { getAuth } from 'firebase/auth';
+
 
 
 function App() {
 
-  const auth = getAuth(App)
+
 
 
   return (
     <div className='w-50 mx-auto'>
 
-          
-       <ReactBootstrap></ReactBootstrap>
+
+       <h1>scacsc</h1>          
+
+
     </div>
   );
 }
